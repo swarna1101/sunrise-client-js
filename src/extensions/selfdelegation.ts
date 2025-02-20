4d5dce3e23b404b1e8651c4370b0d4a76d7a08d0
Senna46 <29295263+Senna46@users.noreply.github.com>
2025-02-20 17:40:46 +0900

    feat: update query

---
+++ src/extensions/selfdelegation.ts
@@ -0,0 +1,44 @@
+import { createProtobufRpcClient, QueryClient } from "@cosmjs/stargate";
+
+import { queryFactory } from "../internal/query-factory";
+import {
+  QueryLockupAccountsByOwnerRequestSchema,
+  QueryLockupAccountsByOwnerResponseSchema,
+  QueryParamsRequestSchema,
+  QueryParamsResponseSchema,
+  QuerySelfDelegationProxyAccountByOwnerRequestSchema,
+  QuerySelfDelegationProxyAccountByOwnerResponseSchema,
+} from "../types/sunrise/selfdelegation";
+
+export type SelfDelegationExtension = ReturnType<typeof setupSelfDelegationExtension>;
+
+export function setupSelfDelegationExtension(base: QueryClient) {
+  const rpc = createProtobufRpcClient(base);
+  const service = "sunrise.selfdelegation.Query";
+
+  return {
+    selfdelegation: {
+      params: queryFactory(
+        rpc,
+        service,
+        "Params",
+        QueryParamsRequestSchema,
+        QueryParamsResponseSchema,
+      ),
+      selfDelegationProxyAccountByOwner: queryFactory(
+        rpc,
+        service,
+        "SelfDelegationProxyAccount",
+        QuerySelfDelegationProxyAccountByOwnerRequestSchema,
+        QuerySelfDelegationProxyAccountByOwnerResponseSchema,
+      ),
+      lockupAccountsByOwner: queryFactory(
+        rpc,
+        service,
+        "LockupAccountsByOwner",
+        QueryLockupAccountsByOwnerRequestSchema,
+        QueryLockupAccountsByOwnerResponseSchema,
+      ),
+    },
+  };
+}
